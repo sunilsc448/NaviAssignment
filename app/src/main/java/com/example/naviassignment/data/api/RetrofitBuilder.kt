8462b36eee0de68d18e52e2af507cb626b97c1b6
sunil_sc <sunil.sc@paytm.com>
2022-08-07 11:41:48 +0530

    Added Retrofit API Service

---
+++ app/src/main/java/com/example/naviassignment/data/api/RetrofitBuilder.kt
@@ -0,0 +1,14 @@
+package com.example.naviassignment.data.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = ""
+class RetrofitBuilder {
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
