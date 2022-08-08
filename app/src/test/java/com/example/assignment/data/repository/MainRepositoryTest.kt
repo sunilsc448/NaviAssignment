fac8aa671ecdfe2d87827902fa657fd392dc426c
sunil_sc <sunil.sc@paytm.com>
2022-08-08 18:36:50 +0530

    Repository Unit test complete

--- app/src/test/java/com/example/assignment/data/repository/MainRepositoryTest.kt
+++ app/src/test/java/com/example/assignment/data/repository/MainRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.example.assignment.data.repository
 
+import android.accounts.NetworkErrorException
 import com.example.assignment.data.api.ApiService
 import com.example.assignment.data.model.PullRequest
 import com.example.assignment.data.model.User
@@ -9,24 +10,24 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import java.io.IOException
 
 class MainRepositoryTest : TestCase() {
-    private val dispatcher = TestCoroutineDispatcher()
-
     private lateinit var SUT: MainRepository
-    private lateinit var apiService: SuccessDataService
+
+    private val dispatcher = TestCoroutineDispatcher()
+    private lateinit var apiService: ApiService
 
     @Before
     override fun setUp() {
         MockitoAnnotations.initMocks(this)
-        apiService = SuccessDataService()
-        SUT = MainRepository(apiService)
     }
 
     @Test
     fun testGetClosedPullRequestsSuccessCase() {
+        apiService = SuccessDataService()
+        SUT = MainRepository(apiService)
         val page = 1
         runBlocking {
             val result = SUT.getClosedPullRequests(page, dispatcher)
@@ -34,17 +35,50 @@ class MainRepositoryTest : TestCase() {
         }
     }
 
+    @Test
+    fun testGetClosedPullRequestsNetworkErrorCase() {
+        apiService = NetworkErrorDataService()
+        SUT = MainRepository(apiService)
+        val page = 1
+        runBlocking {
+            val result = SUT.getClosedPullRequests(page, dispatcher)
+            assertEquals(result.status, Status.NETWORK_ERROR)
+        }
+    }
+
+    @Test
+    fun testGetClosedPullRequestsAPIErrorCase() {
+        apiService = APIErrorDataService()
+        SUT = MainRepository(apiService)
+        val page = 1
+        runBlocking {
+            val result = SUT.getClosedPullRequests(page, dispatcher)
+            assertEquals(result.status, Status.API_ERROR)
+        }
+    }
+
     class SuccessDataService:ApiService{
         val mockedPullRequestsData = listOf(PullRequest(title = "Pull request 1",
-            user = User(user_name = "Sunil", avatar = ""),
-            created_at = "08-08-2022", merged_at = "08-08-2022"),
-            PullRequest(title = "Pull request 2",
-                user = User(user_name = "Sunil", avatar = ""),
-                created_at = "08-08-2022", merged_at = "08-08-2022") )
+                                                user = User(user_name = "Sunil", avatar = ""),
+                                                created_at = "08-08-2022", merged_at = "08-08-2022"),
+                                            PullRequest(title = "Pull request 2",
+                                                user = User(user_name = "Sunil", avatar = ""),
+                                                created_at = "08-08-2022", merged_at = "08-08-2022") )
         override suspend fun getClosedPullRequests(page: Int): List<PullRequest> {
             return mockedPullRequestsData
         }
+    }
 
+    class NetworkErrorDataService:ApiService{
+        override suspend fun getClosedPullRequests(page: Int): List<PullRequest> {
+            throw NetworkErrorException()
+        }
+    }
+
+    class APIErrorDataService:ApiService{
+        override suspend fun getClosedPullRequests(page: Int): List<PullRequest> {
+            throw IOException()
+        }
     }
 }
 
